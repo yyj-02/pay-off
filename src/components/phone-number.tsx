e98a2ff1edbc4f4c7f84e79ea0e0dea4053f5341
yyj-02 <yeohyongjie@outlook.com>
2024-07-03 23:51:41 +0800

    Add phone input

--- src/components/phone-number.tsx
+++ src/components/phone-number.tsx
@@ -7,9 +7,12 @@ import {
   DrawerTitle,
 } from "@/components/ui/drawer";
 import React, { useEffect, useState } from "react";
+import { getUser, updateUserPhoneNumber } from "@/models/userModel";
 
 import { Button } from "@/components/ui/button";
-import { getUser } from "@/models/userModel";
+import { Info } from "lucide-react";
+import { PhoneInput } from "@/components/ui/phone-input";
+import { isValidPhoneNumber } from "react-phone-number-input";
 import { toast } from "@/components/ui/use-toast";
 
 type PhoneNumberProps = {
@@ -24,25 +27,51 @@ const PhoneNumber: React.FC<PhoneNumberProps> = ({
   onOpenChange,
 }) => {
   const [number, setNumber] = useState<string | null | undefined>();
+  const [numberInput, setNumberInput] = useState<string>("");
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  const fetchNumber = async () => {
+    if (!uid) return;
+
+    const user = await getUser(uid);
+    setNumber(user?.phoneNumber);
+  };
 
   useEffect(() => {
-    const fetchNumber = async () => {
-      if (!uid) return;
-
-      const user = await getUser(uid);
-      setNumber(user?.phoneNumber);
-    };
-
     fetchNumber();
   }, [uid]);
 
-  const handleButtonClick = () => {
+  const handleButtonClick = async () => {
     if (number) {
       navigator.clipboard.writeText(number);
 
       toast({
         variant: "default",
         title: "Number copied to clipboard",
+      });
+    } else if (isValidPhoneNumber(numberInput) && uid) {
+      try {
+        setIsDisabled(true);
+        await updateUserPhoneNumber(uid, numberInput);
+        fetchNumber();
+
+        toast({
+          variant: "default",
+          title: "Phone number updated",
+        });
+      } catch (error) {
+        toast({
+          variant: "destructive",
+          title: "Failed to update phone number",
+        });
+      } finally {
+        setIsDisabled(false);
+        setNumberInput("");
+      }
+    } else {
+      toast({
+        variant: "destructive",
+        title: "Invalid phone number",
       });
     }
   };
@@ -66,12 +95,34 @@ const PhoneNumber: React.FC<PhoneNumberProps> = ({
               {number ? (
                 <p className="text-3xl tracking-wide">{number}</p>
               ) : (
-                <></>
+                <div className="grid w-full max-w-sm items-center gap-1.5">
+                  <PhoneInput
+                    placeholder="Enter a phone number"
+                    value={numberInput}
+                    onChange={setNumberInput}
+                    disabled={isDisabled}
+                  />
+                </div>
               )}
             </div>
           </div>
           <DrawerFooter>
-            <Button onClick={handleButtonClick}>
+            {number && (
+              <div className="flex align-top justify-center gap-1 text-muted-foreground">
+                <Info className="mt-px h-3.5 w-3.5 shrink-0" />
+                <p className="text-xs">
+                  Email the phone number to{" "}
+                  <a
+                    href={`mailto:yeohyongjie@outlook.com?subject=SMS Feature Access Request: ${number}`}
+                    className="underline"
+                  >
+                    yeohyongjie@outlook.com
+                  </a>{" "}
+                  to access the SMS feature. to access the SMS feature.
+                </p>
+              </div>
+            )}
+            <Button onClick={handleButtonClick} disabled={isDisabled}>
               {number ? "Copy" : "Submit"}
             </Button>
           </DrawerFooter>
